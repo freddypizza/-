18e59fea093440e83d62a711140cdb9471920511
freddypizza <freddypizza675@gmail.com>
2026-05-16 10:42:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -33,15 +33,15 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def update_l(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.y > 5:
-            self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win_height - 80:
-            self.rect.y += self.speed
-    def update_r(self):
-        keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
         if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
 
 raket1 = Player(img_rocket, 5, 100, 80, 100, 5)
